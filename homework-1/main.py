bc7e8b02f24caf770c4cf6002c6d832e328048b5
Katya Smirnova <vismanork@gmail.com>
2024-02-12 17:58:53 +0300

    homework-1 was completely done

--- homework-1/main.py
+++ homework-1/main.py
@@ -19,29 +19,34 @@ if __name__ == '__main__':
       "id": "UC-OVMPlMA3-YCIeg4z5z23A",
       "snippet": {
         "title": "MoscowPython",
-        "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)",
+        "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\n
+        Присоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)",
         "customUrl": "@moscowdjangoru",
         "publishedAt": "2012-07-13T09:48:44Z",
         "thumbnails": {
           "default": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s88-c-k-c0x00ffffff-no-rj",
+            "url": 
+            "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s88-c-k-c0x00ffffff-no-rj",
             "width": 88,
             "height": 88
           },
           "medium": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s240-c-k-c0x00ffffff-no-rj",
+            "url": 
+            "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s240-c-k-c0x00ffffff-no-rj",
             "width": 240,
             "height": 240
           },
           "high": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s800-c-k-c0x00ffffff-no-rj",
+            "url": 
+            "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s800-c-k-c0x00ffffff-no-rj",
             "width": 800,
             "height": 800
           }
         },
         "localized": {
           "title": "MoscowPython",
-          "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
+          "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\n
+          Присоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
         },
         "country": "RU"
       },
--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+import json
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +8,13 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.api_key = os.getenv('YT_API_KEY')  # Получаем ключ из переменных окружения
+        self.channel = self.youtube().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=4, ensure_ascii=False))
+
+    def youtube(self):
+        return build('youtube', 'v3', developerKey=self.api_key)
