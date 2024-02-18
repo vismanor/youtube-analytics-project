56b9ef5d7b4edfeaa5d97b9396f82bef1e8f91c0
Katya Smirnova <vismanork@gmail.com>
2024-02-18 04:57:04 +0300

    homework-2 is done!

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 import json
 import os
 from googleapiclient.discovery import build
+from dotenv import load_dotenv
 
 
 class Channel:
@@ -9,12 +10,35 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.api_key = os.getenv('YT_API_KEY')  # Получаем ключ из переменных окружения
-        self.channel = self.youtube().channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.channel_id = channel_id
+        self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/'+self.__channel_id
+        self.subscriber_counter = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=4, ensure_ascii=False))
 
-    def youtube(self):
-        return build('youtube', 'v3', developerKey=self.api_key)
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, value):
+        self.__channel_id = value
+
+    @classmethod
+    def get_service(cls):
+        load_dotenv()
+        api_key = os.getenv('YT_API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, filename):
+        data = self.__dict__
+        del (data['channel'])
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
